e980ad7742d12077e76d9aae081a1fbce12e2d6f
PAVITRA BEHARA <pavitraredmi@gmail.com>
2024-04-06 18:35:30 +0530

    SET: Next App setup done!

---
+++ client/src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className='h-screen w-screen flex justify-center items-center bg-neutral-950'>
+      <div className='p-3 border border-neutral-800 bg-neutral-900 text-md font-bold text-neutral-400 rounded-lg shadow'>
+        Setup Done!!
+      </div>
+    </main>
+  );
+}
