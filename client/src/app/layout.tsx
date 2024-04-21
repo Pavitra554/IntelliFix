3efa381f74bb459ccddbce918c56b9b6da68a60e
PAVITRA BEHARA <pavitraredmi@gmail.com>
2024-04-21 20:47:04 +0530

    ADD: created initial hero page

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,8 +1,9 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
+import type { Metadata } from "next";
+import { Open_Sans } from "next/font/google";
+import { Providers } from "./providers";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Open_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -15,8 +16,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang='en'>
+      <body className={inter.className}>
+        <Providers>{children}</Providers>
+      </body>
     </html>
   );
 }
---
+++ client/src/app/main/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className='h-screen w-screen flex justify-center items-center bg-neutral-950'>
+      <div className='p-3 border border-neutral-800 bg-neutral-900 text-md font-bold text-neutral-400 rounded-lg shadow'>
+        Main Page!!
+      </div>
+    </main>
+  );
+}
