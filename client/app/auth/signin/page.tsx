880cef1adbeb2003a4004658067854304de5adf8
Pavitra554 <pavitraredmi@gmail.com>
2024-05-12 21:37:23 +0530

    ADD: New login page added

--- client/app/auth/signin/page.tsx
+++ client/app/auth/signin/page.tsx
@@ -13,7 +13,7 @@ export default function Signin() {
       <div className="absolute h-full opacity-30 w-full bg-gradient-to-r from-indigo-700 to-purple-700 blur-3xl animate-pulse duration-2000"></div>
       <div className='relative dark:bg-zinc-950/80 bg-zinc-100 max-w-md  w-full flex flex-col gap-2 items-center p-6 border rounded-lg shadow-lg'>
           <Image src={'/logo.svg'} height={80} width={80} alt="Logo" className="drop-shadow-xl mt-6" />
-          <div className='text-lg md:text-4xl font-extrabold tracking-wide mt-4'>Welcome to IntelliFix</div>
+          <div className='text-lg md:text-4xl font-extrabold tracking-wide mt-4`'>Welcome to IntelliFix</div>
           <div className='text-zinc-500 text-center text-xs md:text-base'>
             Where code meets precision effortlessly. Experience seamless debugging with AI-powered optimization. Join us and elevate your coding journey. Welcome to a new era of development
           </div>
