638bf77e8062626cca4b765fcfb94e0c88b6af49
Pavitra554 <pavitraredmi@gmail.com>
2024-05-14 23:43:09 +0530

    FIX: Bug fix

--- client/components/sidebar/sidebar.tsx
+++ client/components/sidebar/sidebar.tsx
@@ -10,7 +10,7 @@ export default function SideBar() {
     const[ mode ,setMode] = React.useState("debugger")
     const { data: session } = useSession()
     return (
-        <section className="flex max-w-72 w-full flex-col justify-between border-r p-3">
+        <section className="flex bg-zinc-950 max-w-72 w-full flex-col justify-between border-r p-3">
             <div>
                 <button className="flex flex-row items-center gap-3 w-full border rounded-lg p-3 hover:bg-zinc-950 hover:text-white dark:hover:bg-zinc-800 ease-linear duration-100 ">
                     <Image src={'/logo.svg'} height={25} width={25} alt="Logo" className="drop-shadow-xl" />
@@ -35,7 +35,7 @@ export default function SideBar() {
                 <div className="w-full flex flex-row gap-3 border rounded-lg p-3 hover:bg-zinc-200 dark:hover:bg-zinc-800 ease-linear duration-100 truncate">
                     <Avatar className="border">
                         <AvatarImage src={session?.user?.image || ""} alt="user" />
-                        <AvatarFallback>CN</AvatarFallback>
+                        <AvatarFallback>{session?.user?.name?.charAt(0)}</AvatarFallback>
                     </Avatar>
                     <div>
                         <div className="font-semibold">{session?.user?.name}</div>
