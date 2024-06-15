81d1e6330b2e6d2142095aface565a419c91bb5e
Barun Hazary <barunhazaryofficial@gmail.com>
2024-06-15 20:39:20 +0530

    added CORS

--- server/main.py
+++ server/main.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import user
 
-
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # This allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # This allows all methods (GET, POST, etc.)
+    allow_headers=["*"],  # This allows all headers
+)
 
 
 @app.get("/")
